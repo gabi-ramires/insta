057695acace10f90be5fb79f98813e9f4efab20c
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-08-02 23:41:30 -0300

    vue instalado

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,31 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+document.addEventListener('deviceready', function () {
+    // Vue
+    new Vue({
+        el: '#appVue',
+        data: {
+            nome: 'gabs'
+        },
+        created() {
+            //alert('oi')
+        },
+        methods: {
+            compartilhar(){
+                var imageUrl = 'https://www.gstatic.com/webp/gallery/2.jpg';
+                window.plugins.socialsharing.share(
+                    null,             // Mensagem (ou null se não precisar)
+                    null,             // Assunto (ou null se não precisar)
+                    imageUrl,         // URL da imagem
+                    null,             // URL do link (ou null se não precisar)
+                    function (success) {
+                        alert('Compartilhado com sucesso!');
+                    },
+                    function (error) {
+                        alert('Erro ao compartilhar: ' + error);
+                    }
+                );
+            }
+        }
+    });
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+}, false);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
